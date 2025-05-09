ae189e63fd5f9d083e301458ef729acc14167df5
EJTUF\Ervin <ervinjs06@gmail.com>
2025-05-08 20:22:39 -0700

    Added fs and Joi objects. Created chemicalCollection. Added images fake path. Added ejs engine and changed default views path. Added sds route that pulls chemicalNames from the collection to create links to sds pages. Added subroute of sds that returns the sds sheet page. The subroute ensures that only chemical names contained within the db will return a page. Created support function createLetterGroups to convert an array of strings into an alphabetical order single character array with only unique elements.  Boostrap version is updated; links have been updated with the latest ones. Directory routes for the navbar and template loader have been corrected. Functionality to show the SDSs within the database has been completed.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const bcrypt = require("bcrypt");
 const path = require("path");
+const fs = require("fs");
+const Joi = require("joi");
 
-require("dotenv").config();
+
 const app = express();
 const port = 8000;
 const saltRounds = 12;
@@ -13,12 +17,14 @@ const saltRounds = 12;
 const connectDB = require("./databaseConnection");
 
 async function setupServer() {
-    const database = await connectDB(); // Wait for database connection
-    const userCollection = database.collection("users"); // Now this works!
+    const database = await connectDB(); 
+    const userCollection = database.collection("users"); 
+    const chemicalCollection = database.collection("chemicals");
 
     // Middleware Setup 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    
 
     const mongoStore = MongoStore.create({
         mongoUrl: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/sessions`,
@@ -35,7 +41,11 @@ async function setupServer() {
     // Serve Static Files
     app.use("/images", express.static(path.join(__dirname, "images")));
     app.use("/public", express.static(path.join(__dirname, "public")));
+    app.use("/images", express.static(path.join(__dirname, "images")));
     app.use("/app", express.static(path.join(__dirname, "app")));
+
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "app", "views"));
 
     // Root Route
     app.get("/", (req, res) => {
@@ -44,7 +54,6 @@ async function setupServer() {
         }
         res.redirect("/main");
     });
-
 
     // Register Route
     app.post("/register", async (req, res) => {
@@ -101,15 +110,45 @@ async function setupServer() {
     });
     
     // SDS Route
-    app.get("/sds", (req, res) => {
-        res.sendFile(path.join(__dirname, "app", "html", "sds.html"));
+    app.get("/sds", async (req, res) => {
+        if (!req.session.user) {
+            return res.redirect("/login");
+        }
+        
+        const chemicalNames = await chemicalCollection.find({}).project({substance_name:1, _id:1}).toArray();
+        let letterGroups = createLetterGroups(chemicalNames.map((c) => c.substance_name));
+        res.render("sds", {chemicalNames:chemicalNames, letterGroups:letterGroups});
+    });
+
+    app.get("/sds/:substance_name", async (req, res) => {
+        if (!req.session.user) {
+            return res.redirect("/login");
+        }
+
+        var substance_name = req.params.substance_name;
+
+        const schema = Joi.string().max(30).required();
+        const validationResult = schema.validate(substance_name);
+        if(validationResult.error != null)
+        {
+            res.redirect("/sds");
+            return;
+        }
+
+        const chemicalJSON = await chemicalCollection.find({substance_name: substance_name}).toArray();
+        if(!chemicalJSON[0])
+        {
+            res.redirect("/sds");
+            return;
+        }
+
+        res.render("sdsTemplate", {chemicalJSON: chemicalJSON[0]});
     });
 
     app.get("*dummy", (req, res) => {
         res.status = 404;
         res.send("Page not found - 404");
     });
-
 
     // Start Server
     app.listen(port, () => {
@@ -118,3 +157,28 @@ async function setupServer() {
 }
 
 setupServer();
+
+/**
+ * Returns an array of the different letter groupings of chemicals, based on their first 
+ * letter.
+ * @param {Array<String>} chemicalNames represents an array of all chemical names to be 
+ *                                      listed out on the page.
+ * @returns a String array of letters sorted in alphabetical order that represents the 
+ *          different letter groups of chemicals.
+ */
+function createLetterGroups(chemicalNames)
+{
+    let nonUniqueFirstChars = chemicalNames.map((name) => name.toUpperCase().substring(0, 1));
+
+    let uniqueAlpha = [];
+    nonUniqueFirstChars.forEach((letter) => {
+        if(!uniqueAlpha.includes(letter))
+        {
+            uniqueAlpha.push(letter);
+        }
+    });
+
+    let letterGroups = uniqueAlpha.sort();
+
+    return letterGroups;
+}
